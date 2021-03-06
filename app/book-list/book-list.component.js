04ca7fafd1920e2b99fdbf9d4e3297b2ab75b346
Andrew <ando.vanweerden@gmail.com>
2021-03-06 13:54:57 +0800

    Added header titles that sort books when clicked

--- app/book-list/book-list.component.js
+++ app/book-list/book-list.component.js
@@ -6,6 +6,16 @@ angular.
     templateUrl: 'book-list/book-list.template.html',
     controller: ['$http', function bookListController($http) {
       let self = this;
+      self.orderProp = 'finished';
+
+      self.setOrderProp = function(field) {
+        // If already selected, display in descending order
+        if (self.orderProp == field) {
+          self.orderProp = '-' + field;
+        } else {
+          self.orderProp = field;
+        }
+      }
 
       $http.get('./books/books.json').
         then(response => self.books = response.data);
